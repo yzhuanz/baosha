b6b9cb1e6ac50bc48ec091b35956bf87d0d51645
9157832 <kuka@2021>
2023-03-17 09:18:26 +0800

    init

---
+++ src/main/java/com/example/baosha/BaoshaApplication.java
@@ -0,0 +1,13 @@
+package com.example.baosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoshaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaoshaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baosha/BaoshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baosha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaoshaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
