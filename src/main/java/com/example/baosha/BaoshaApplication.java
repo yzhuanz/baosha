23338982b497b9864f1c576ba4dcade78c4ebb55
9157832 <kuka@2021>
2023-03-22 17:22:01 +0800

    init

--- src/main/java/com/example/baosha/BaoshaApplication.java
+++ src/main/java/com/example/baosha/BaoshaApplication.java
@@ -1,9 +1,11 @@
 package com.example.baosha;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.baosha.pojo")
 public class BaoshaApplication {
 
     public static void main(String[] args) {
